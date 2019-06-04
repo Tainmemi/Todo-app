860979ed6aa846a0773e91ed1998b6157b4267f2
Tainmemi <pimnipa.sakunchaisiriwit@mail.kmutt.ac.th>
2019-06-04 17:44:50 +0700

    Create project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,30 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'todo-task';
+  newTask: string;
+  taskErrorMessage = false;
+  tasks = [];
+  delTask: string;
+  i: number;
+  finTask = false;
+
+  addTask(){
+    if(this.newTask){
+      this.tasks.push(this.newTask);
+      this.taskErrorMessage = false;
+      this.newTask = '';
+      
+    }
+    else{
+      this.taskErrorMessage = true;
+    }
+  }
+  
+  deleteTask(){
+    this.tasks.splice(this.i,1);
+  }
+
+  finishTask(){
+    
+  }
 }
